df009a732e058b28b178d510bc9250e72d31fa45
YairCarpin <yair.carpinteiro100@gmail.com>
2026-03-01 15:49:24 -0600

    actualizacion de bug

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-numero_1 = int(input("agrega un numero"))
-numero_2 = int(input("agrega otro numero"))
+numero_uno = int(input("agrega un numero"))
+numero_dos = int(input("agrega otro numero"))
 
-resultado = numero_1 + numero_2
+resultado = numero_uno + numero_dos
 
 print(fr"el resultado es: {resultado}")
